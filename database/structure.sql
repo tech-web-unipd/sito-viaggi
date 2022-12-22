5e855f2d3db93a7829b7edd052cdd7214e1ea41c
SinicatoNicola <sinickinfoutili@gmail.com>
2022-12-22 09:36:06 +0100

    Aggiornamento database

--- database/structure.sql
+++ database/structure.sql
@@ -11,7 +11,6 @@ DROP TABLE IF EXISTS Purchase;
 DROP TABLE IF EXISTS Turist;
 DROP TABLE IF EXISTS Image_Activity;
 DROP TABLE IF EXISTS Image_Destination;
-DROP TABLE IF EXISTS Image_Travel;
 DROP TABLE IF EXISTS Image_Airline;
 DROP TABLE IF EXISTS Image_Hotel;
 
@@ -35,8 +34,8 @@ CREATE TABLE Do(
     destination int,
     activity int,
     PRIMARY KEY(destination, activity),
-    FOREIGN KEY(destination) REFERENCES Destination(id),
-    FOREIGN KEY(activity) REFERENCES Activity(id)
+    FOREIGN KEY(destination) REFERENCES Destination(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(activity) REFERENCES Activity(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Airline(
@@ -47,8 +46,8 @@ CREATE TABLE Flight(
     destination int,
     airline varchar(100),
     PRIMARY KEY(destination, airline),
-    FOREIGN KEY(destination) REFERENCES Destination(id),
-    FOREIGN KEY(airline) REFERENCES Airline(name)
+    FOREIGN KEY(destination) REFERENCES Destination(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(airline) REFERENCES Airline(name) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Hotel(
@@ -62,30 +61,23 @@ ALTER TABLE Hotel AUTO_INCREMENT=00001;
 CREATE TABLE Travel(
     destination int,
     start_date DATE,
-    end_date DATE,
+    end_date DATE, 
     price float not null,
-    /* id int PRIMARY KEY AUTO_INCREMENT,
-    destination int not null,
-    start_date date not null,
-    end_date date not null,
-    PROBLEMA POSSIAMO AVERE IDEALMENTE TANTI VIAGGI CON LA STESSA DATA D'INIZIO, A MENO CHE NON METTIAMO ANCHE GLI ALTRI 
-    TRE ATTRIBUTI PER FARE UNA SUPERCHIAVE MA POI NON SO SE BASTA PASSARE SOLO L'ID PER LE IMMAGINI
-    */
     PRIMARY KEY(destination, start_date, end_date),
-    FOREIGN KEY(destination) REFERENCES Destination(id)
+    FOREIGN KEY(destination) REFERENCES Destination(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-/*ALTER TABLE Travel AUTO_INCREMENT=00001;*/
 
 CREATE TABLE User(
     username varchar(50) PRIMARY KEY,
     name varchar(100) not null,
     surname varchar(100) not null,
-    gender char not null,
+    gender char not null CHECK (gender IN ("U","D","A"));,
     date_of_birth DATE not null,
-    pw_hash varchar(100) not null,
+    pw_hash varchar(100) not null, 
     email varchar(100) not null,
     numero varchar(20) not null,
     admin boolean not null
+    /*img VARCHAR(256) NOT NULL DEFAULT '<rootFolder />/img/utenti/def1.png' PER METTERE L'IMMAGINE DI DEFAULT*/
 );
 
 CREATE TABLE Payment_Method(
@@ -101,8 +93,8 @@ CREATE TABLE Purchase(
     moment timestamp not null,
     username varchar(50) not null,
     card varchar(16) not null,
-    FOREIGN KEY(username) REFERENCES User(username),
-    FOREIGN KEY(card) REFERENCES Payment_Method(card_number)
+    FOREIGN KEY(username) REFERENCES User(username) ON DELETE NO ACTION, 
+    FOREIGN KEY(card) REFERENCES Payment_Method(card_number) ON DELETE NO ACTION
 );
 ALTER TABLE Purchase AUTO_INCREMENT=00001;
 
@@ -110,12 +102,12 @@ CREATE TABLE Turist(
     fiscal_code varchar(16) PRIMARY KEY, 
     name varchar(100) not null,
     surname varchar(100) not null,
-    gender char not null,
+    gender char not null CHECK (gender IN ("U","D","A"));,
     date_of_birth DATE not null,
     email varchar(100) not null,
     numero varchar(20) not null.
     purchase int not null,
-    FOREIGN KEY(purchase) REFERENCES Purchase(id)
+    FOREIGN KEY(purchase) REFERENCES Purchase(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE Image_Activity(
@@ -124,7 +116,7 @@ CREATE TABLE Image_Activity(
     alt varchar(100) not null,
     activity int,
     PRIMARY KEY(id,activity),
-    FOREIGN KEY(activity) REFERENCES Activity(id)
+    FOREIGN KEY(activity) REFERENCES Activity(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 ALTER TABLE Image_Activity AUTO_INCREMENT=00001;
 
@@ -134,26 +126,9 @@ CREATE TABLE Image_Destination(
     alt varchar(100) not null,
     destination int,
     PRIMARY KEY(id,destination),
-    FOREIGN KEY(destination) REFERENCES Destination(id)
+    FOREIGN KEY(destination) REFERENCES Destination(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 ALTER TABLE Image_Destination AUTO_INCREMENT=00001;
-
-CREATE TABLE Image_Travel(
-    id int AUTO_INCREMENT,
-    path varchar(100) not null,
-    alt varchar(100) not null,
-    destination int,
-    start_date date,
-    end_date date,
-    /* travel int, */
-    PRIMARY KEY(id,destination, start_date, end_date),
-    FOREIGN KEY(destination) REFERENCES Destination(id),
-    FOREIGN KEY(start_date) REFERENCES Travel(start_date),
-    FOREIGN KEY(end_date) REFERENCES Travel(end_date)
-    /*PRIMARY KEY (id, travel),
-    FOREIGN KEY (travel) REFERENCES Travel(id)*/
-);
-ALTER TABLE Image_Travel AUTO_INCREMENT=00001;
 
 CREATE TABLE Image_Airline(
     id int AUTO_INCREMENT,
@@ -161,16 +136,16 @@ CREATE TABLE Image_Airline(
     alt varchar(100) not null,
     airline int,
     PRIMARY KEY(id,airline),
-    FOREIGN KEY(airline) REFERENCES Airline(name)
+    FOREIGN KEY(airline) REFERENCES Airline(name) ON DELETE CASCADE ON UPDATE CASCADE
 );
 ALTER TABLE Image_Airline AUTO_INCREMENT=00001;
 
 CREATE TABLE Image_Hotel(
     id int AUTO_INCREMENT,
-    path varchar(100) not null,
+    path varchar(10 0) not null,
     alt varchar(100) not null,
     hotel int,
     PRIMARY KEY(id,hotel),
-    FOREIGN KEY(hotel) REFERENCES Hotel(id)
+    FOREIGN KEY(hotel) REFERENCES Hotel(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 ALTER TABLE Image_Hotel AUTO_INCREMENT=00001;
