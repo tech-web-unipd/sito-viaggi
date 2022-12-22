4d8324a71de86479f1d1595f04251b7d5de96b7c
Enrico <enrizangri@gmail.com>
2022-12-22 13:03:24 +0100

    Aggiunta implementazione Image

--- app/Image.php
+++ app/Image.php
@@ -1,8 +1,20 @@
 <?php
-
-namespace app;
-
 class Image
 {
+    private string $path;
+    private string $alt;
 
+    public function __construct(string $path, string $alt)
+    {
+        $this->path = $path;
+        $this->alt = $alt;
+    }
+
+    /**
+     * @return string The HTML image with src and alt attributes
+     */
+    public function build(): string
+    {
+        return "<img src=\"$this->path\" alt=\"$this->alt\">";
+    }
 }
