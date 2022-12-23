d8ec137489e9782ff46455bec66aadd086660747
Enrico <enrizangri@gmail.com>
2022-12-23 17:20:03 +0100

    Creata AbstractComponent
    AbstractComponent è la classe sulla quale si baseranno tutte le componenti simili e ripetute del sito come Hotel Destination ed Activity
    
    Ora la classe activity deriva da AbstractComponent

---
+++ app/AbstractComponent.php
@@ -0,0 +1,127 @@
+<?php
+require_once './lib/DatabaseLayer.php';
+require_once './app/Image.php';
+class IdNotDefined extends Exception {
+    public function __construct() {
+        parent::__construct("Id not defined");
+    }
+}
+class FieldNotLoaded extends Exception {
+    public function __construct($field) {
+        parent::__construct("Field $field not loaded");
+    }
+}
+class UndefinedField extends Exception {
+    public function __construct() {
+        parent::__construct("There are on or more fields not defined");
+    }
+}
+abstract class AbstractComponent
+{
+    protected ?string $id;
+    protected ?string $name;
+    protected ?string $description;
+    private string $image_table;
+    private string $image_foreign_key;
+    protected ?array $images;
+    protected ?Image $cover;
+
+    public function __construct(string $image_table, string $image_foreign_key, string $id = null, string $name = null, string $description = null, array $images = null, Image $cover = null)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->image_table = $image_table;
+        $this->image_foreign_key = $image_foreign_key;
+        $this->images = $images;
+        $this->cover = $cover;
+    }
+
+    public abstract function loadFromDatabase(DatabaseLayer $db): void;
+
+    public abstract function insertIntoDatabase(DatabaseLayer $db): void;
+
+    /**
+     * @throws IdNotDefined if the id value is null
+     * @throws Exception in case of errors with database communication
+     */
+    protected function loadImages(DatabaseLayer $db): void {
+        if($this->id === null) {
+            throw new IdNotDefined();
+        }
+        $this->images = array();
+        $results = $db->executeStatement("SELECT * FROM ".$this->image_table." WHERE ".$this->image_foreign_key." = ?", [$this->id]);
+        foreach ($results as $row) {
+            $image = new Image($row['path'], $row['alt'], $row['is_cover']);
+            $this->images[] = $image;
+            if($image->is_cover) {
+                $this->cover = $image;
+            }
+        }
+    }
+
+    /**
+     * @throws UndefinedField if there are on or more fields not defined
+     * @throws Exception in case of errors with database communication
+     */
+    protected function insertImagesIntoDatabase(DatabaseLayer $db): void {
+        if (!($this->images == null)) {
+            if($this->id === null) {
+                throw new IdNotDefined();
+            }
+            if($this->cover == null) {
+                $this->cover = $this->images[0];
+                $this->images[0]->is_cover = true;
+            }
+            foreach ($this->images as $image) {
+                $db->executeStatement("INSERT INTO ".$this->image_table." (path, alt, is_cover, ".$this->image_foreign_key.") VALUES (?, ?, ?, ?)", [$image->path, $image->alt, $image->is_cover, $this->id]);
+            }
+        } else {
+            throw new UndefinedField();
+        }
+    }
+
+    /**
+     * @throws FieldNotLoaded if name value is null
+     */
+    public function getName(): string {
+        if($this->name != null) {
+            return $this->name;
+        } else {
+            throw new FieldNotLoaded('name');
+        }
+    }
+
+    /**
+     * @throws FieldNotLoaded if description value is null
+     */
+    public function getDescription(): string {
+        if ($this->description != null) {
+            return $this->description;
+        } else {
+            throw new FieldNotLoaded('description');
+        }
+    }
+
+    /**
+     * @throws FieldNotLoaded if images value is null
+     */
+    public function getImages(): array {
+        if ($this->images != null) {
+            return $this->images;
+        } else {
+            throw new FieldNotLoaded('images');
+        }
+    }
+
+    /**
+     * @throws FieldNotLoaded if cover value is null
+     */
+    public function getCover(): Image {
+        if ($this->cover != null) {
+            return $this->cover;
+        } else {
+            throw new FieldNotLoaded('cover');
+        }
+    }
+}
--- app/Activity.php
+++ app/Activity.php
@@ -1,64 +1,25 @@
 <?php
 require_once './lib/DatabaseLayer.php';
-require_once './app/Image.php';
+require_once './app/AbstractComponent.php';
 
 class ActivityNotFound extends Exception {
     public function __construct($id) {
         parent::__construct("Activity with id: $id not found");
     }
 }
-class FieldNotLoaded extends Exception {
-    public function __construct($field) {
-        parent::__construct("Field $field not loaded");
-    }
-}
-class IdNotDefined extends Exception {
-    public function __construct() {
-        parent::__construct("Id not defined");
-    }
-}
-class UndefinedField extends Exception {
-    public function __construct() {
-        parent::__construct("There are on or more fields not defined");
-    }
-}
-class Activity
+class Activity extends AbstractComponent
 {
-    private string $id;
-    private string $name;
-    private int $price;
-    private string $description;
-    private array $images;
-    private Image $cover;
+    private ?int $price;
 
     public function __construct(string $id = null, string $name = null, int $price = null, string $description = null, array $images = null, Image $cover = null)
     {
-        $this->id = $id;
-        $this->name = $name;
         $this->price = $price;
-        $this->description = $description;
-        $this->images = $images;
-        $this->cover = $cover;
+        parent::__construct("image_activity", "activity", $id, $name, $description, $images, $cover);
     }
 
     public function __toString()
     {
         return "Activity with id: $this->id named $this->name";
-    }
-
-    /**
-     * @throws Exception in case of errors with database communication
-     */
-    private function loadImages(DatabaseLayer $db): void {
-        $this->images = array();
-        $results = $db->executeStatement("SELECT * FROM image_activity WHERE activity = ?", [$this->id]);
-        foreach ($results as $row) {
-            $image = new Image($row['path'], $row['alt'], $row['is_cover']);
-            $this->images[] = $image;
-            if($image->is_cover) {
-                $this->cover = $image;
-            }
-        }
     }
 
     /**
@@ -84,27 +45,10 @@ class Activity
     }
 
     /**
-     * @throws Exception in case of errors with database communication
-     */
-    private function insertImagesIntoDatabase(DatabaseLayer $db): void {
-        if (!($this->images == null)) {
-            if($this->cover == null) {
-                $this->cover = $this->images[0];
-                $this->images[0]->is_cover = true;
-            }
-            foreach ($this->images as $image) {
-                $db->executeStatement("INSERT INTO image_activity (path, alt, is_cover, activity) VALUES (?, ?, ?, ?)", [$image->path, $image->alt, $image->is_cover, $this->id]);
-            }
-        } else {
-            throw new UndefinedField();
-        }
-    }
-
-    /**
      * @throws UndefinedField if one or more fields are not defined
      * @throws Exception in case of errors with database communication
      */
-    public function saveIntoDatabase(DatabaseLayer $db): void {
+    public function insertIntoDatabase(DatabaseLayer $db): void {
         if (!($this->name == null || $this->price == null || $this->description == null || $this->images == null)) {
             if($this->id == null) {
                 $last_id = $db->executeStatement("SELECT MAX(id) AS last_id FROM activity");
@@ -119,17 +63,6 @@ class Activity
     }
 
     /**
-     * @throws FieldNotLoaded if name value is null
-     */
-    public function getName(): string {
-        if($this->name != null) {
-            return $this->name;
-        } else {
-            throw new FieldNotLoaded('name');
-        }
-    }
-
-    /**
      * @throws FieldNotLoaded if price value is null
      */
     public function getPrice(): int {
@@ -137,39 +70,6 @@ class Activity
             return $this->price;
         } else {
             throw new FieldNotLoaded('price');
-        }
-    }
-
-    /**
-     * @throws FieldNotLoaded if description value is null
-     */
-    public function getDescription(): string {
-        if ($this->description != null) {
-            return $this->description;
-        } else {
-            throw new FieldNotLoaded('description');
-        }
-    }
-
-    /**
-     * @throws FieldNotLoaded if images value is null
-     */
-    public function getImages(): array {
-        if ($this->images != null) {
-            return $this->images;
-        } else {
-            throw new FieldNotLoaded('images');
-        }
-    }
-
-    /**
-     * @throws FieldNotLoaded if cover value is null
-     */
-    public function getCover(): Image {
-        if ($this->cover != null) {
-            return $this->cover;
-        } else {
-            throw new FieldNotLoaded('cover');
         }
     }
 }
