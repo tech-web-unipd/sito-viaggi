f8d650a74d5a49d8a97425f82d544f29adc581e9
Enrico <enrizangri@gmail.com>
2022-12-31 12:08:52 +0100

    Aggiunte le attività a destinazione
    È stato aggiunto un array alla classe destinazione il quale contiene tutte le attività per quella destinazione

--- src/app/Destination.php
+++ src/app/Destination.php
@@ -10,16 +10,38 @@ class Destination extends AbstractComponent
     private const IMAGE_FOREIGN_KEY = "destination";
     private ?string $continent;
     private ?string $state;
+    private ?array $activities;
 
-    public function __construct(string $id = null, string $name = null, string $description = null, array $images = null, Image $cover = null, string $continent = null, string $state = null) {
+    public function __construct(string $id = null, string $name = null, string $description = null, array $images = null, Image $cover = null, string $continent = null, string $state = null, array $activities = null)
+    {
+        parent::__construct($id, $name, $description, $images, $cover);
         $this->continent = $continent;
         $this->state = $state;
-        parent::__construct(self::IMAGE_TABLE, self::IMAGE_FOREIGN_KEY, $id, $name, $description, $images, $cover);
+        $this->activities = $activities;
     }
 
     public function __toString()
     {
         return "Destination with id: $this->id named $this->name";
+    }
+
+    /**
+     * @throws IdNotDefined if the id value is null
+     * @throws Exception
+     */
+    private function  loadActivities(DatabaseLayer $db): void {
+        if ($this->id === null) {
+            throw new IdNotDefined();
+        }
+
+        $this->images = array();
+        $result = $db->executeStatement("SELECT activity FROM offers WHERE destination = ?", array($this->id));
+
+        foreach ($result as $row) {
+            $activity = new Activity($row['activity']);
+            $activity->loadFromDatabase($db);
+            $this->activities[] = $activity;
+        }
     }
 
     /**
@@ -86,6 +108,17 @@ class Destination extends AbstractComponent
             return $this->state;
         } else {
             throw new FieldNotLoaded('state');
+        }
+    }
+
+    /**
+     * @throws FieldNotLoaded if activities value is null
+     */
+    public function getActivities(): array {
+        if ($this->activities != null) {
+            return $this->activities;
+        } else {
+            throw new FieldNotLoaded('activities');
         }
     }
 }
