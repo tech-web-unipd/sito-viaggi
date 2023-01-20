eff00e104a072b8788af0e91c0d8285ded5a7d3c
Enrico <enrizangri@gmail.com>
2023-01-20 19:00:47 +0100

    Changed db user to sapo

--- src/app/global.php
+++ src/app/global.php
@@ -2,4 +2,4 @@
 
 require_once "lib/DatabaseLayer.php";
 
-$db = new \utilities\DatabaseLayer("127.0.0.1", "zan", "Ollare123", "progetto_tec_web");
+$db = new \utilities\DatabaseLayer("127.0.0.1", "sapo", "Ollare123", "progetto_tec_web");
